26c0dd4fd5a2e9860ae1bd915cfd6583aada4ec7
Raymond Yeh <ray@geek.sg>
2021-09-30 09:50:22 +0800

    feat: simple test

--- src/config.ts
+++ src/config.ts
@@ -11,6 +11,9 @@ const generateNetworks = () => {
     throw new Error(`MUMBAI_ACCOUNT_1 not set in config`);
   return {
     hardhat: {},
+    local: {
+      url: `http://127.0.0.1:8545/`,
+    },
     matic: {
       url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
       accounts: [process.env.MATIC_ACCOUNT_1],
