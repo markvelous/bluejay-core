d190af2d9147b80d73da7987106773f62ae69698
Raymond Yeh <ray@geek.sg>
2021-09-30 14:57:06 +0800

    fix: contract code

---
+++ contracts/core/contracts/CollateralJoin.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+interface TokenLike {
+  function decimals() external view returns (uint8);
+
+  function transfer(address, uint256) external returns (bool);
+
+  function transferFrom(
+    address,
+    address,
+    uint256
+  ) external returns (bool);
+}
+
+interface LedgerLike {
+  function modifyCollateral(
+    bytes32,
+    address,
+    int256
+  ) external;
+}
+
+contract CollateralJoin is Initializable {
+  mapping(address => uint256) public authorizedAccounts;
+  LedgerLike public ledger; // CDP Engine
+  bytes32 public collateralType; // Collateral Type
+  TokenLike public collateral;
+  uint256 public decimals;
+  uint256 public live; // Active Flag
+
+  // --- Events ---
+  event GrantAuthorization(address indexed account);
+  event RevokeAuthorization(address indexed account);
+  event Deposit(address indexed user, uint256 amount);
+  event Withdraw(address indexed user, uint256 amount);
+
+  function initialize(
+    address ledger_,
+    bytes32 collateralType_,
+    address collateral_
+  ) public initializer {
+    authorizedAccounts[msg.sender] = 1;
+    live = 1;
+    ledger = LedgerLike(ledger_);
+    collateralType = collateralType_;
+    collateral = TokenLike(collateral_);
+    decimals = collateral.decimals();
+    emit GrantAuthorization(msg.sender);
+  }
+
+  // --- Auth ---
+  function grantAuthorization(address user) external isAuthorized {
+    authorizedAccounts[user] = 1;
+    emit GrantAuthorization(user);
+  }
+
+  function revokeAuthorization(address user) external isAuthorized {
+    authorizedAccounts[user] = 0;
+    emit RevokeAuthorization(user);
+  }
+
+  modifier isAuthorized() {
+    require(
+      authorizedAccounts[msg.sender] == 1,
+      "CollateralJoin/not-authorized"
+    );
+    _;
+  }
+
+  function shutdown() external isAuthorized {
+    live = 0;
+  }
+
+  function deposit(address user, uint256 wad) external {
+    require(live == 1, "CollateralJoin/not-live");
+    require(int256(wad) >= 0, "CollateralJoin/overflow");
+    ledger.modifyCollateral(collateralType, user, int256(wad));
+    require(
+      collateral.transferFrom(msg.sender, address(this), wad),
+      "CollateralJoin/failed-transfer"
+    );
+    emit Deposit(user, wad);
+  }
+
+  function withdraw(address user, uint256 wad) external {
+    require(wad <= 2**255, "CollateralJoin/overflow");
+    ledger.modifyCollateral(collateralType, msg.sender, -int256(wad));
+    require(collateral.transfer(user, wad), "CollateralJoin/failed-transfer");
+    emit Withdraw(user, wad);
+  }
+}
---
+++ contracts/core/contracts/OracleRelayer.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+interface LedgerLike {
+  function updateSafetyPrice(bytes32 collateralType, uint256 data) external;
+}
+
+interface OracleLike {
+  function getPrice() external returns (uint256, bool); // wad
+}
+
+contract OracleRelayer is Initializable {
+  uint256 constant ONE = 10**27;
+
+  struct CollateralType {
+    OracleLike oracle; // Price Feed
+    uint256 collateralizationRatio; // Liquidation ratio [ray]
+  }
+
+  mapping(address => uint256) public authorizedAccounts;
+  mapping(bytes32 => CollateralType) public collateralTypes;
+
+  LedgerLike public ledger; // CDP Engine
+  uint256 public redemptionPrice; // ref per dai [ray]
+  uint256 public live;
+
+  // --- Events ---
+  event GrantAuthorization(address indexed account);
+  event RevokeAuthorization(address indexed account);
+  event UpdateCollateralPrice(
+    bytes32 collateralType,
+    uint256 price, // [wad]
+    uint256 safetyPrice // [ray]
+  );
+  event UpdateParameter(bytes32 indexed parameter, uint256 data);
+  event UpdateParameter(
+    bytes32 indexed parameter,
+    bytes32 indexed collateralType,
+    uint256 data
+  );
+  event UpdateParameter(
+    bytes32 indexed parameter,
+    bytes32 indexed collateralType,
+    address data
+  );
+
+  // --- Init ---
+  function initialize(address ledger_) public initializer {
+    authorizedAccounts[msg.sender] = 1;
+    ledger = LedgerLike(ledger_);
+    redemptionPrice = ONE;
+    live = 1;
+    emit GrantAuthorization(msg.sender);
+  }
+
+  // --- Auth ---
+  function grantAuthorization(address user) external isAuthorized {
+    authorizedAccounts[user] = 1;
+    emit GrantAuthorization(user);
+  }
+
+  function revokeAuthorization(address user) external isAuthorized {
+    authorizedAccounts[user] = 0;
+    emit RevokeAuthorization(user);
+  }
+
+  modifier isAuthorized() {
+    require(
+      authorizedAccounts[msg.sender] == 1,
+      "OracleRelayer/not-authorized"
+    );
+    _;
+  }
+
+  // --- Math ---
+  function rdiv(uint256 x, uint256 y) internal pure returns (uint256 z) {
+    z = (x * ONE) / y;
+  }
+
+  // --- Administration ---
+  function updateOracle(bytes32 collateralType, address oracle_)
+    external
+    isAuthorized
+  {
+    collateralTypes[collateralType].oracle = OracleLike(oracle_);
+    emit UpdateParameter("oracle", collateralType, oracle_);
+  }
+
+  function updateRedemptionPrice(uint256 data) external isAuthorized {
+    redemptionPrice = data;
+    emit UpdateParameter("redemptionPrice", data);
+  }
+
+  function updateCollateralizationRatio(bytes32 collateralType, uint256 data)
+    external
+    isAuthorized
+  {
+    require(data >= ONE, "OracleRelayer/ratio-lt-ray");
+    collateralTypes[collateralType].collateralizationRatio = data;
+    emit UpdateParameter("collateralizationRatio", collateralType, data);
+  }
+
+  // --- Update value ---
+  function updateCollateralPrice(bytes32 collateralType) external {
+    (uint256 price, bool isValidPrice) = collateralTypes[collateralType]
+      .oracle
+      .getPrice();
+    uint256 safetyPrice = isValidPrice
+      ? rdiv(
+        rdiv(price * 10**9, redemptionPrice),
+        collateralTypes[collateralType].collateralizationRatio
+      )
+      : 0;
+    ledger.updateSafetyPrice(collateralType, safetyPrice);
+    emit UpdateCollateralPrice(collateralType, price, safetyPrice);
+  }
+
+  function shutdown() external isAuthorized {
+    live = 0;
+  }
+}
