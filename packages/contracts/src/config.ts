149de367d7c9674a90782fc682ea5193e303d04a
Raymond Yeh <ray@geek.sg>
2021-10-07 14:29:32 +0800

    fix: remove required keys

--- packages/contracts/src/config.ts
+++ packages/contracts/src/config.ts
@@ -2,15 +2,10 @@ import { config as loadEnv } from "dotenv";
 
 loadEnv();
 
+const PLACEHOLDER_KEY =
+  "0000000000000000000000000000000000000000000000000000000000000001";
+
 const generateNetworks = () => {
-  if (!process.env.ALCHEMY_KEY)
-    throw new Error(`ALCHEMY_KEY not set in config`);
-  if (!process.env.STAGING_KEY)
-    throw new Error(`STAGING_KEY not set in config`);
-  if (!process.env.PRODUCTION_KEY)
-    throw new Error(`PRODUCTION_KEY not set in config`);
-  if (!process.env.DEVELOPMENT_KEY)
-    throw new Error(`DEVELOPMENT_KEY not set in config`);
   return {
     hardhat: {},
     local: {
@@ -18,17 +13,17 @@ const generateNetworks = () => {
     },
     development: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
-      accounts: [process.env.DEVELOPMENT_KEY],
+      accounts: [process.env.DEVELOPMENT_KEY ?? PLACEHOLDER_KEY],
       chainId: 80001,
     },
     staging: {
       url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
-      accounts: [process.env.STAGING_KEY],
+      accounts: [process.env.STAGING_KEY ?? PLACEHOLDER_KEY],
       chainId: 137,
     },
     production: {
       url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
-      accounts: [process.env.PRODUCTION_KEY],
+      accounts: [process.env.PRODUCTION_KEY ?? PLACEHOLDER_KEY],
       chainId: 137,
     },
   };
