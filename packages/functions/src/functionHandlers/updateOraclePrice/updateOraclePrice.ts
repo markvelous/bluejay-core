3b13866ffdbc8a174c5d710e630400e077ee31dd
yehjxraymond <yehjxraymond@gmail.com>
2021-10-12 18:32:49 +0800

    feat: update price to telegram (#13)
    Co-authored-by: Raymond Yeh <ray@geek.sg>

--- packages/functions/src/functionHandlers/updateOraclePrice/updateOraclePrice.ts
+++ packages/functions/src/functionHandlers/updateOraclePrice/updateOraclePrice.ts
@@ -5,6 +5,7 @@ import { publicRequestHandler } from "../../middlewares/handlers";
 import { getLogger } from "../../common/logger";
 import { getPricesFromMultipleSources } from "./getPricesFromMultipleSources";
 import { config } from "../../config";
+import { sendMessage } from "../../common/telegram";
 
 const { info } = getLogger("updateOraclePrice");
 
@@ -20,13 +21,43 @@ const wallet = new Wallet(config.wallets.oraclePriceFeed, provider) as any;
 // eslint-disable-next-line @typescript-eslint/camelcase,@typescript-eslint/camelcase
 const OracleContract = SingleFeedOracle__factory.connect(config.contracts.oracle, wallet);
 
+const broadcastPrice = (
+  priceData: {
+    sources: {
+      source: string;
+      rate: number;
+    }[];
+    median: {
+      source: string;
+      rate: number;
+    };
+  },
+  hash: string
+) => {
+  const { median, sources } = priceData;
+  let message = "";
+  message += `📈 *Oracle Price Update* 📈\n`;
+  message += `Median source: ${median.source}\n`;
+  message += `Median price: ${median.rate}\n\n`;
+  message += `🧾 *Sources* 🧾\n`;
+  sources.forEach(({ source, rate }) => {
+    message += `Source: ${source}\n`;
+    message += `Rate: ${rate}\n\n`;
+  });
+  message += `📡 *Price Update* 📡\n`;
+  message += `[${hash}](https://polygonscan.com/tx/${hash})`;
+  sendMessage(message);
+};
+
 const handleUpdateOraclePrice = async () => {
   const priceData = await getPricesFromMultipleSources();
   const price = priceData.median.rate;
   info(`Median price: ${price}`);
+
   const priceAsBn = BigNumber.from(Math.floor(price * 100000)).mul(exp(18 - 5));
   const receipt = await OracleContract.setPrice(priceAsBn, { gasPrice: utils.parseUnits("30", "gwei") });
   info(`Waiting to be mined: ${receipt.hash}`);
+  broadcastPrice(priceData, receipt.hash);
 
   return { hash: receipt.hash };
 };
