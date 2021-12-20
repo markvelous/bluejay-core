dc39fe2dc1a13e4795926bcae1c21db405c2ac7b
Raymond Yeh <ray@geek.sg>
2021-12-20 23:11:35 +0800

    feat: added token addresses

--- packages/app-vault/src/components/ControlPanelContainer/ControlPanelContainer.tsx
+++ packages/app-vault/src/components/ControlPanelContainer/ControlPanelContainer.tsx
@@ -8,6 +8,8 @@ export const ControlPanelContainer: FunctionComponent = () => {
     <Layout>
       <Content>
         <div>Addresses</div>
+        <div>BLU Token: {deployments.contracts.BluejayToken.address}</div>
+        <div>DAI Token: {deployments.contracts.MockReserveToken.address}</div>
         <div>Uniswap (BLU/DAI): {deployments.contracts.IUniswapV2Pair.address}</div>
         <div>Bond Depository (DAI): {deployments.contracts.BondDepositoryImpl.address}</div>
       </Content>
